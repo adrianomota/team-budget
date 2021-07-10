2cd65f9d79698b40364f1a8b04e56df2734e5991
adrianomota <adrianowsh@hotmail.com>
2021-07-10 12:54:48 -0300

    initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,22 @@
+# Changelog
+Tidas as alteracões notáveis neste projeto serão documentadas neste projeto
+
+Formato baseado em [Keep a Changelog](https://keepchangelog.com/en/1.0.0
+E este projeto segue [Semantic Versioning(https://semver.org/spec/v2.0.0/html)
+
+## [.0.0.1] - 28-02-2021
+
+
+### unrelease
+- CRUD de posts no blog
+- Criacão e autenticacãp e autorixacão no google
+- Comentários
+- Leitura de dados do html
+
+
+### Added
+- Projeto inicial
+- Configuracão do Credo para garantir qualidade
+- Configuracão do Sobelow para seguranca
+- Configuracão do coveralls para os testes
+- Inclusão de arquivos para processe de qualidade de projeto
---
+++ mix.exs
@@ -0,0 +1,91 @@
+defmodule TeamBudget.MixProject do
+  use Mix.Project
+
+  @github_url "https://github.com/adrianomota/team-budget"
+
+  def project do
+    [
+      app: :team_budget,
+      version: "0.1.0",
+      elixir: "~> 1.12.1",
+      description: "Projeto GraphQl em Elixir",
+      source_utl: @github_url,
+      homepage_url: @github_url,
+      files: ~w(mix.exs lib LICENSE.md README.md CHANGELOG.md),
+      package: [
+        maintainers: ["Adriano de LIma Araujo Mota"],
+        license: ["MIT"],
+        links: %{
+          "github" => @github_url
+        }
+      ],
+      docs: [
+        main: "readme",
+        extras: ["readme.md", "CHANGELOG.md"]
+      ],
+      elixirc_paths: elixirc_paths(Mix.env()),
+      compilers: [:phoenix, :gettext] ++ Mix.compilers(),
+      start_permanent: Mix.env() == :prod,
+      aliases: aliases(),
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.json": :test,
+        "coveralls.html": :test
+      ]
+    ]
+  end
+
+  # Configuration for the OTP application.
+  #
+  # Type `mix help compile.app` for more information.
+  def application do
+    [
+      mod: {TeamBudget.Application, []},
+      extra_applications: [:logger, :runtime_tools]
+    ]
+  end
+
+  # Specifies which paths to compile per environment.
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
+
+  # Specifies your project dependencies.
+  #
+  # Type `mix help deps` for examples and options.
+  defp deps do
+    [
+      {:phoenix, "~> 1.5.9"},
+      {:phoenix_ecto, "~> 4.1"},
+      {:ecto_sql, "~> 3.4"},
+      {:postgrex, ">= 0.0.0"},
+      {:phoenix_live_dashboard, "~> 0.4"},
+      {:telemetry_metrics, "~> 0.4"},
+      {:telemetry_poller, "~> 0.4"},
+      {:gettext, "~> 0.11"},
+      {:jason, "~> 1.0"},
+      {:plug_cowboy, "~> 2.0"},
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
+      {:sobelow, "~> 0.8", only: :dev},
+      {:excoveralls, "~> 0.14.0", only: :test}
+    ]
+  end
+
+  # Aliases are shortcuts or tasks specific to the current project.
+  # For example, to install project dependencies and perform other setup tasks, run:
+  #
+  #     $ mix setup
+  #
+  # See the documentation for `Mix` for more info on aliases.
+  defp aliases do
+    [
+      setup: ["deps.get", "ecto.setup"],
+      "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
+      "ecto.reset": ["ecto.drop", "ecto.setup"],
+      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"]
+    ]
+  end
+end
